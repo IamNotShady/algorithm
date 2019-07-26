308de96a9882ca03a85554c8c735ad79eb7b8641
IamNotShady <zhou.xiaoxiao@outlook.com>
2019-07-26 09:16:46 +0800

    init

---
+++ src/ownership.rs
@@ -0,0 +1,112 @@
+fn main1() {
+    let s = String::from("hello"); // s 进入作用域
+
+    takes_ownership(s); // s 的值移动到函数里 ...
+                        // ... 所以到这里不再有效
+    let x = 5; // x 进入作用域
+
+    makes_copy(x); // x 应该移动函数里，
+                   // 但 i32 是 Copy 的，所以在后面可继续使用 x
+    println!("{}", x);
+} // 这里, x 先移出了作用域，然后是 s。但因为 s 的值已被移走，
+  // 所以不会有特殊操作
+
+fn takes_ownership(some_string: String) {
+    // some_string 进入作用域
+    println!("{}", some_string);
+} // 这里，some_string 移出作用域并调用 `drop` 方法。占用的内存被释放
+
+fn makes_copy(some_integer: i32) {
+    // some_integer 进入作用域
+    println!("{}", some_integer);
+} // 这里，some_integer 移出作用域。不会有特殊操作
+
+fn main2() {
+    let s1 = gives_ownership(); // gives_ownership 将返回值
+                                // 移给 s1
+
+    let s2 = String::from("hello"); // s2 进入作用域
+
+    let s2 = takes_and_gives_back(s2); // s2 被移动到
+    println!("s1 = {},s2 = {}", s1, s2);
+    // takes_and_gives_back 中,
+    // 它也将返回值移给 s3
+} // 这里, s3 移出作用域并被丢弃。s2 也移出作用域，但已被移走，
+  // 所以什么也不会发生。s1 移出作用域并被丢弃
+
+fn gives_ownership() -> String {
+    // gives_ownership 将返回值移动给
+    // 调用它的函数
+
+    let some_string = String::from("hello"); // some_string 进入作用域.
+
+    some_string // 返回 some_string 并移出给调用的函数
+}
+
+// takes_and_gives_back 将传入字符串并返回该值
+fn takes_and_gives_back(a_string: String) -> String {
+    // a_string 进入作用域
+
+    a_string // 返回 a_string 并移出给调用的函数
+}
+
+fn main3() {
+    let mut s1 = String::from("hello");
+    let len = calculate_length(&s1);
+    println!("The length of '{}' is {}.", s1, len);
+    change(&mut s1);
+    let r1 = &s1;
+    let r2 = &s1;
+    dangle();
+    println!("{}, {}", r1, r2);
+}
+
+fn calculate_length(s: &String) -> usize {
+    s.len()
+}
+
+fn change(some_string: &mut String) {
+    some_string.push_str(", world");
+}
+
+fn dangle() -> String {
+    let s = String::from("hello");
+    s
+}
+fn main() {
+    let my_string = String::from("hello world");
+
+    // first_word 中传入 `String` 的 slice
+    let word = first_word(&my_string[..]);
+
+    let my_string_literal = "hello world";
+
+    // first_word 中传入字符串字面值的 slice
+    let word = first_word(&my_string_literal[..]);
+
+    // 因为字符串字面值 **就是** 字符串 slice，
+    // 这样写也可以，即不使用 slice 语法！
+    let word = first_word(my_string_literal);
+    let user1 = User {
+        email: String::from("someone@example.com"),
+        username: String::from("someusername123"),
+        active: true,
+        sign_in_count: 1,
+    };
+}
+fn first_word(s: &str) -> &str {
+    let bytes = s.as_bytes();
+    for (i, &item) in bytes.iter().enumerate() {
+        if item == b' ' {
+            return &s[0..i];
+        }
+    }
+    &s[..]
+}
+
+struct User {
+    username: String,
+    email: String,
+    sign_in_count: u64,
+    active: bool,
+}
